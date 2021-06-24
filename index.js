5b67cc322b1cc3bdc90060683d8e0370605f56d3
Leo Long <yujunlong2000@gmail.com>
2021-06-24 13:54:24 +0800

    仅在需要是添加外部样式辅助

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ var footnoteConfig = config.footnote || {};
 
 // Register footnotes filter
 hexo.extend.filter.register('before_post_render', function(data) {
-  data.content = renderFootnotes(data.content, footnoteConfig);
+  var content = renderFootnotes(data.content, footnoteConfig);
+  if (data.content != content) {
+  	data.content = content;
+  	data.render_footnote = true;
+  }
   return data;
 });
 
 // Add CDN CSS resources
 hexo.extend.filter.register('after_post_render', function(data) {
-  data.content =
+  if (data.render_footnote) {
+    data.content =
       util.htmlTag('link', {rel: 'stylesheet', type: 'text/css', href: 'https://cdn.jsdelivr.net/hint.css/2.4.1/hint.min.css'}) +
       data.content;
-  return data;
+    data.render_footnote = undefined;
+    return data;
+  }
 });
